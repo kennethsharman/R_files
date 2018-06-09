7f19da6ef44347363406bd9847f83af2eb5d3646
Ken Sharman <kenneth.sharman@ucalgary.ca>
2018-06-08 22:34:49 -0600

    testing merge

--- chapter_4_questions.R
+++ chapter_4_questions.R
@@ -5,5 +5,11 @@
 # Question 4.7
 # Y ~ Binom( n = 10, p = 0.2)
 # P( 2 < Y < 5)
-(120*0.2^3*0.8^7) + (210 * 0.2^4 * 0.8^6)
+(120 * 0.2^3 * 0.8^7) + (210 * 0.2^4 * 0.8^6)
 dbinom( x = 3, size = 10, prob = 0.2) + dbinom( x = 4, size = 10, prob = 0.2)
+# P( 2 <= Y < 5)
+(45 * 0.2^2 * 0.8^8) + (120 * 0.2^3 * 0.8^7) + (210 * 0.2^4 * 0.8^6)
+dbinom( x = 2, size = 10, prob = 0.2) + 
+  dbinom( x = 3, size = 10, prob = 0.2) + dbinom( x = 4, size = 10, prob = 0.2)
+sum( dbinom( x = c(2,3,4), size = 10, prob = 0.2) )
+
