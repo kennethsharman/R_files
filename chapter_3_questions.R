4539f7f09a5f2f8a222a13f1d050dbdd9fc670a8
Ken Sharman <kenneth.sharman@ucalgary.ca>
2018-06-11 01:00:27 -0600

    Working on discrete quantiles

--- chapter_3_questions.R
+++ chapter_3_questions.R
@@ -3,6 +3,23 @@
 # June 6, 2018
 # Kenneth Sharman
 
+# Question 3.22
+# A single fair die is tossed one. Let Y be number facing up.
+# Find expected value and variance of Y
+p <- rep(1/6, 6)
+y <- seq.int(1,6,1)
+m <- sum(y*p)
+v <- sum(y^2*p) - m^2
+
+# Question 3.43
+# Y ~ Binom( p = 0.7, n = 5 )
+# P( Y = 5 )
+dbinom( x = 5, size = 5, prob = 0.7 )
+# P( Y >= 4 ) = 1 - P( Y <= 3 )
+dbinom( x = 4, size = 5, prob = 0.7) + dbinom( x = 5, size = 5, prob = 0.7 )
+1 - pbinom( q = 3, size = 5, prob = 0.7)
+pbinom( q = 3, size = 5, prob = 0.7, lower.tail = FALSE)
+
 # Quesiton 3.73
 # Geometric Distribution, p = 0.9
 # P( Y = 3 ) number of failures before 1st success is x = 2
@@ -12,6 +29,20 @@ dgeom( x = (3-1), p = 0.9 )
 1 - dgeom( x = 1, p = 0.9 ) - dgeom( x = 0, p = 0.9 )
 1 - pgeom( q = 1, 0.9 )
 pgeom( q = 1, p = 0.9, lower.tail = FALSE )
+
+# Question 3.45
+# Y ~ Binom( p = 0.8, n = 3)
+# P( Y >= 1 ) = 1 - P( Y = 0 )
+pbinom( q = 0, size = 3, prob = 0.8, lower.tail = FALSE)
+1 - dbinom( x = 0, size = 3, prob = 0.8)
+
+# Question 3.73
+# Y ~ Geo( p = 0.9)
+# P( Y = 3 )
+dgeom( x = 2, prob = 0.9)
+# P( Y >= 3 ) = 1- P ( Y <= 2 )
+1 - pgeom( q = 1, prob = 0.9)
+pgeom( q = 1, prob = 0.9, lower.tail = FALSE)
 
 # Question 3.75
 # Y ~ Geo( p = 0.1 )
