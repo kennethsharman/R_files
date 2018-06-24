3d94776a04708fb6b255c55722cfba2505089f7e
Ken Sharman <kenneth.sharman@ucalgary.ca>
2018-06-24 16:21:00 -0600

    A few questions from chapter 6

---
+++ chapter_6_questions.R
@@ -0,0 +1,11 @@
+# Question 6.43
+# Transform sample mean to standard normal then calc prob
+# Z ~ Norm( 1, 0 )
+# P ( |Ybar - mean| < 1 ) n = 25, var = 16
+1 - 2 * pnorm( (-5/4), 0, 1)
+# P ( |Ybar - mean| < 1 ) n = 36, var = 16
+1 - 2 * pnorm( (-6/4), 0, 1)
+# P ( |Ybar - mean| < 1 ) n = 64, var = 16
+1 - 2 * pnorm( -2, 0, 1 )
+# P ( |Ybar - mean| < 1 ) n = 81, var = 16
+1 - 2 * pnorm( -9/2, 0, 1 )
