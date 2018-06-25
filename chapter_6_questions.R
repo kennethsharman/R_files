8fae4fc1aa59181b61a1b9b68b939341e16c2e12
Ken Sharman <kenneth.sharman@ucalgary.ca>
2018-06-24 19:25:20 -0600

    A few more questions from chapter 6 and 7

--- chapter_6_questions.R
+++ chapter_6_questions.R
@@ -9,3 +9,16 @@
 1 - 2 * pnorm( -2, 0, 1 )
 # P ( |Ybar - mean| < 1 ) n = 81, var = 16
 1 - 2 * pnorm( -9/2, 0, 1 )
+
+# Question 6.45
+# P ( Z > (u-182)/sqrt(12.61) )
+z_0.99 = qnorm(0.99, 0, 1)
+( z_0.99* sqrt(12.61) ) + 182
+
+# Question 6.55
+# Y1 ~ Pois(7) and T2 ~ Pois(7)
+# U = Y1 + Y2
+# P( U >= 20) = 1 - P( U <= 19 )
+1 - ppois(19, lambda = 14)
+ppois(19, lambda = 14, lower.tail = FALSE)
+
---
+++ chapter_7_questions.R
@@ -0,0 +1,41 @@
+# Example 7.2
+1 - 2 * pnorm(-0.9, 0, 1)
+
+# Example 7.3
+x = qnorm( (0.05/2), 0, 1)
+( x / -0.3 )^2
+
+# Question 7.9 a
+# n = 16 P ~ Norm( mean, sd=1 )
+# P( |Ybar - mean| < 0.3 )
+1 - 2 * pnorm(-1.2, 0, 1)
+
+# Question 7.9 b
+1 - 2 * pnorm( -1.5, 0, 1 )
+1 - 2 * pnorm( -1.8, 0, 1 )
+1 - 2 * pnorm( -2.1, 0, 1 )
+1 - 2 * pnorm( -2.4, 0, 1 )
+
+# QUestion 7.11
+1 - 2 * pnorm( -1.5, 0, 1)
+
+# Example 7.8
+pnorm( -2.5, 0, 1)
+
+# Example 7.9
+pnorm(-3, 0, 1)
+
+# Question 7.43
+1 - 2 * pnorm( -2, 0, 1)
+
+# Question 7.45
+pnorm( -1.6, 0, 1)
+
+# Example 7.10
+1 - pnorm( 1, 0, 1)
+
+# Question 7.73
+pnorm( 1.27, 0, 1)
+
+# Question 7.75
+1 - 2 * pnorm( -1.2, 0, 1)
