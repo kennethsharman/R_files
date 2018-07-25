a16115a4083d56331d81c3b69cd7c5f10ff8e38a
Kenneth Sharman <kenneth.sharman@ucalgary.ca>
2018-07-24 23:20:47 -0600

    Chapt 8 init

---
+++ chapter_8_questions.R
@@ -0,0 +1,10 @@
+# Wackerly Mathematical Statistics w/ app. 7th ed.
+# Chapter 8 Questions
+# Started June 24, 2018
+# Updated End of July, 2018
+# Kenneth Sharman
+
+# Example from notes
+z = qnorm(p = 1 - 0.025, 0, 1)
+177 - z * ( 6 / sqrt(9) )
+177 + z * ( 6 / sqrt(9) )
