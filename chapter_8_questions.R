d6e0e3f2458e235c039c60b2d5e5024b17b92ec9
Kenneth Sharman <kenneth.sharman@ucalgary.ca>
2018-07-25 23:26:47 -0600

    More q's

--- chapter_8_questions.R
+++ chapter_8_questions.R
@@ -5,6 +5,20 @@
 # Kenneth Sharman
 
 # Example from notes
-z = qnorm(p = 1 - 0.025, 0, 1)
-177 - z * ( 6 / sqrt(9) )
-177 + z * ( 6 / sqrt(9) )
+z1 = qnorm(p = 1 - 0.025, 0, 1)
+177 - z1 * ( 6 / sqrt(9) )
+177 + z1 * ( 6 / sqrt(9) )
+
+z2 = qnorm(p = 1 - 0.05, 0, 1)
+177 - z2 * (6 / sqrt(9) )
+177 + z2 * (6 / sqrt(9) )
+
+( (z1*6) / 0.5 )^2
+
+# Example A
+z1 = qnorm(p = 1 - 0.025, 0, 1)
+p_hat = 202/1010
+p_hat - z1 * sqrt( (p_hat * (1-p_hat) ) / ( 1010 ) )
+p_hat + z1 * sqrt( (p_hat * (1-p_hat) ) / ( 1010 ) )
+
+(z1/0.01)^2 * 0.5 * (1-0.5)
